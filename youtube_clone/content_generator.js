be5f2a1a398ee8f9a73d3b33414f02c39ccb2624
Dipesh Thakur <letsmaildipesh1@gmail.com>
2026-03-11 11:52:13 +0545

    Using JS to generate HTMl for videos
    Using one file to store array of objects which has
     info on video data and using another js file for
     video html generation.

---
+++ youtube_clone/content_generator.js
@@ -0,0 +1,37 @@
+import {videoInfo} from "./video_info.js";
+
+let html = "";
+
+videoInfo.forEach(video=>{
+  html += `
+    <div class="video-container">
+      <img src="${video.thumbnail}" class="thumbnail-pic">
+      <div class="video-info-container">
+        <div class="channelpic-channeltooltip-container">
+          <img src="${video.channelPic}" class="channel-pic">
+          <div class="channel-tooltip">
+            <img src="${video.channelPic}" class="channel-tooltip-pic">
+            <div class="channel-tooltip-data-container">
+              <span class="channel-tooltip-name">
+                ${video.channelName}
+              </span>
+              <span class="channel-tooltip-subscribers">${video.subscribers}</span>
+            </div>
+          </div>
+        </div>
+        
+        <div class="video-info">
+          <p class="video-title">${video.videoTitle}</p>
+          <div class="video-stats">
+            <p class="channel-name">${video.channelName}</p>
+            <div class="stats">
+              <span class="views">${video.views}</span> &middot; <span class="age">${video.time}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  `
+});
+const container = document.querySelector('.video-grid');
+container.innerHTML = html;
---
+++ youtube_clone/video_info.js
@@ -0,0 +1,12 @@
+export const videoInfo = [
+  {
+    thumbnail: "./thumbnails/thumbnail-1.webp",
+    channelPic:"./channel-pictures/channel-pic1.jpg",
+    channelName:"SuperSimpleDev",
+    subscribers: "741K subscribers",
+    videoTitle:"HTML & CSS Full Course - Beginner to Pro",
+    views:"17M views",
+    time:"3 years ago",
+
+  }
+]
